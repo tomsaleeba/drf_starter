d36b6a625de7c807813f154e54a61b2264fb97ab
Tom Saleeba <tom.saleeba@gmail.com>
2019-02-19 18:24:18 +1030

    feat: add coreapi API documentation

--- drf_starter/urls.py
+++ drf_starter/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from dynamic_rest import routers
+from rest_framework.documentation import include_docs_urls
 
 from drf_starter.the_app import viewsets
 
@@ -13,4 +14,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(title='DRF_Starter API', public=False))
 ]
