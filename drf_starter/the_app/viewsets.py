1a78b5b232030cbf002c811a0c0605621c160fc5
Tom Saleeba <tom.saleeba@gmail.com>
2019-02-22 08:34:06 +1030

    feat: add default version to other models

--- drf_starter/the_app/viewsets.py
+++ drf_starter/the_app/viewsets.py
@@ -4,6 +4,15 @@ from dynamic_rest import viewsets
 from drf_starter.the_app import models
 from drf_starter.the_app import serializers
 
+def only_one_version_handler(version, the_serializer):
+    default_version = '1'
+    the_version = version or default_version
+    try:
+        assert the_version == '1'
+        return the_serializer
+    except AssertionError:
+        raise NotAcceptable(detail="Supplied version '%s' is not supported. Valid versions are %s" % (the_version,
+            str(list(default_version))))
 
 class UserViewSet(viewsets.DynamicModelViewSet):
     serializer_class = serializers.UserBaseSerializer
@@ -29,8 +38,14 @@ class GroupViewSet(viewsets.DynamicModelViewSet):
     queryset = models.Group.objects.all()
     ordering = ('-id',)
 
+    def get_serializer_class(self):
+        return only_one_version_handler(self.request.version, serializers.GroupSerializer)
+
 
 class LocationViewSet(viewsets.DynamicModelViewSet):
     serializer_class = serializers.LocationSerializer
     queryset = models.Location.objects.all()
     ordering = ('-id',)
+
+    def get_serializer_class(self):
+        return only_one_version_handler(self.request.version, serializers.LocationSerializer)
